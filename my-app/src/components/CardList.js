dbd4fa2d50bc38ab0cde8e5e316366c971abed53
AA <alessandro.aglietti@gmail.com>
2023-02-16 23:42:28 +0000

    fix: clean up card list ui

--- my-app/src/components/CardList.js
+++ my-app/src/components/CardList.js
@@ -7,22 +7,11 @@ export default function CardList() {
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {cards.map((product) => (
                 <div key={product.id} className="group relative">
-                    <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                    <div className="min-h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none">
                         <img
                             src={product.imageSrc}
                             alt={product.imageAlt}
-                            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                         />
-                    </div>
-                    <div className="mt-4 flex justify-between">
-                        <div>
-                            <h3 className="text-sm text-gray-700">
-                                <a href={product.href}>
-                                    <span aria-hidden="true" className="absolute inset-0" />
-                                    {product.name}
-                                </a>
-                            </h3>
-                        </div>
                     </div>
                 </div>
             ))}
